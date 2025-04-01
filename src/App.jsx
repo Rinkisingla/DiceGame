7eeea9d033bfd3660c5d2a6a24562b2fc6858fa0
Rinkisingla <singlarinki13@gmail.com>
2025-04-01 18:57:05 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
    return (
     <>
       {
-            // isGameStarted ?<Gameplay/>:<Startgame toggle={toggleGameplay}/>
+             isGameStarted ?<Gameplay/>:<Startgame toggle={toggleGameplay}/>
       } 
-      <Gameplay/>
+      
     </>
   )
 }
